8266f6b28fb4719a8136965b372df34a1a71c57e
mappuboy <pituvaraliyange@gmail.com>
2024-07-13 08:48:58 +0530

    locators

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ import java.time.Duration;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
        /* //Invoking the browser
 
@@ -103,6 +103,86 @@ public class Main {
         //if it is on the above the line it effect to line below
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
+
+
+           /*using link locator
+        ===================
+         to use link text locator it should need link ("a href")*/
+        driver.findElement(By.linkText("Forgot your password?")).click();
+
+        Thread.sleep(2000);
+          /*using xpath locator
+        ===================
+         //tagname[@attribute = 'value']
+         to validate xpath on th console = $x('//input[@placeholder='Name']')
+         */
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("ebuilder");
+        /*using xpath locator
+        ===================
+         //tagname[@attribute = 'value']
+         to validate xpath on th console = $x('//input[@placeholder='Name']')
+          and also clear the text box using clear method to clear the textbox
+          Problem if there is an attributes with multiple elements - using indexing method to uniqualy point the value //input[@type='text'][2]
+         */
+
+        driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("jhone@sa.com");
+        driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+
+        //using css with indexing method
+        //indexing methd is not recomending
+        driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("jhone@sca.com");
+
+        //xpath with parent to child using html tag (traverse) with indexing method
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("0123456789");
+
+        /*clicking the reset button
+       // driver.findElement(By.xpath("//form/div/button[2]")).click();
+       Error happens because it is not in the stable state it is in the transition state with moving this pade to next page
+       using code 114 -> Thread.sleep method to stop the execution of code
+       after clicking the forget password link it will stop to give some time
+       Error msg displaying -ElementClickInterceptedException: element click intercepted: Element <button
+       class="reset-pwd-btn">...</button> is not clickable at point (577, 437). Other element would receive the
+       click: <button class="go-to-login-btn">...</button>
+
+       */
+       driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+
+        //grab the infield validation by parent child traverse through csslocator
+        //assigning extracted value to variable
+        String password = driver.findElement(By.cssSelector("form p")).getText();
+        System.out.println(password);
+
+        //redirect to login page
+        driver.findElement(By.cssSelector("button.go-to-login-btn")).click();
+
+        //to get xpath every element with parent to child traverse
+        //Customize xpath - //div[@class='forgot-pwd-btn-conainer']/button[1]
+        //Parent - //div[@class='forgot-pwd-btn-conainer']
+        //Child - /button[1]
+        //travers - first button [1]
+        //div[@class='forgot-pwd-btn-conainer']/button[1]
+        driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+
+        //stop the execute code section for 2 sec
+        Thread.sleep(2000);
+
+        //hard coding the username and password
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("Rahul");
+        //using css locator with regular expression, by using "*" to state the static word part
+        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+
+        //select the checkbox
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.name("chkboxTwo")).click();
+
+        //click the button
+        //using xpath locator with regular expression, by using "contains" keyword to state the static word part
+        driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+
+        //To check the if the loaded page
+        System.out.println("Landed page title : "+driver.getTitle());
+        System.out.println("Landed page URL : "+driver.getCurrentUrl());
 
     }
 }
