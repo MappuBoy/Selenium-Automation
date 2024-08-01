5377dc6b7800616f1efe4fe1711ee4cad3b2d274
mappuboy <pituvaraliyange@gmail.com>
2024-08-01 12:11:41 +0530

    Dynamic dropdown using parent to chlid xpath traverse

--- src/main/java/org/example/AutomationInTheWebPages.java
+++ src/main/java/org/example/AutomationInTheWebPages.java
@@ -13,33 +13,33 @@ public class AutomationInTheWebPages {
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
         //driver.manage().window().fullscreen();
 
-        Thread.sleep(500);
+        /*Thread.sleep(500);
 
         //static dropdown with "Select" tag to access that user Select in build class
-        /*if the dropdown creates with select tag then it is a static dropdown there of options indexes are not going
+        *//*if the dropdown creates with select tag then it is a static dropdown there of options indexes are not going
         to change 1 option -> 0th Index
         2 option -> 1st Index
         select option using selectByIndex() to select the option
         This methods working if there using "Select" tag
-         */
+         *//*
         WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
         Select dropdown =new Select(staticDropdown);
         dropdown.selectByIndex(3);
 
-        /*by using this method it returns the web element of the selected option and by using getText method it will
+        *//*by using this method it returns the web element of the selected option and by using getText method it will
          extract the selected values text
-        */
+        *//*
         System.out.println(dropdown.getFirstSelectedOption().getText());
 
-        /*select option using selectByVisibleText() to select the option from the visible option on the UI
-         */
+        *//*select option using selectByVisibleText() to select the option from the visible option on the UI
+         *//*
         Thread.sleep(500);
         dropdown.selectByVisibleText("AED");
         System.out.println(dropdown.getFirstSelectedOption().getText());
 
         Thread.sleep(500);
-        /*select option using selectByValue() to select the option from the value attribute in the console
-         */
+        *//*select option using selectByValue() to select the option from the value attribute in the console
+         *//*
         dropdown.selectByValue("INR");
         System.out.println(dropdown.getFirstSelectedOption().getText());
         Thread.sleep(500);
@@ -49,24 +49,24 @@ public class AutomationInTheWebPages {
         Thread.sleep(1000);
 
         //1st way
-        /*driver.findElement(By.id("hrefIncAdt")).click();
+        *//*driver.findElement(By.id("hrefIncAdt")).click();
         driver.findElement(By.id("hrefIncAdt")).click();
         driver.findElement(By.id("hrefIncAdt")).click();
-        driver.findElement(By.id("hrefIncAdt")).click();*/
+        driver.findElement(By.id("hrefIncAdt")).click();*//*
 
         //2nd way
-        /*repeat for 3 times using for while loop
+        *//*repeat for 3 times using for while loop
         when we using while loop it will rotate for infinent time but when we use
         this it will stop after loop condition is ture
-        */
+        *//*
 
-        /*//initialize the variable
+        //initialize the variable
         int i =0;
         //comparison
         while (i<4){
             driver.findElement(By.id("hrefIncAdt")).click();
             i++;
-        }*/
+        }*//*
 
         //using for loop for the execution
         for (int i = 0; i < 4; i++) {
@@ -77,17 +77,38 @@ public class AutomationInTheWebPages {
         //when we know exact endpoint of the iteration then using for loop if not we using while loop
 
         System.out.println("After selecting the values : "+driver.findElement(By.id("divpaxinfo")).getText());
+        Thread.sleep(1000); */
 
         //Dynamic dropdowns - These dropdowns displays according to the user selected option
 
         //Xpath ->//a[@value='BLR'],//a[@value='MAA'][2] using "[2]" because need to select it from second option
 
-        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")).click();
-        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+/*        driver.findElement(By.xpath("//select[@id='ctl00_mainContent_ddl_originStation1']")).click();
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//a[@value='MAA']")).click();
+        driver.findElement(By.xpath("(//option[@value='IXG'])")).click();
+        Thread.sleep(1000);
+        //identifing the instances and getting them
+        driver.findElement(By.xpath("//option[@value='MAA'][2]")).click();*/
 
 
+// dynamic dromdown without using indexes
+
+//  //a[@value='MAA']  - Xpath for chennai
+
+//  //a[@value='BLR']
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+
+        Thread.sleep(2000);
+
+//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+        //travese xpath
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+
+        driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active")).click();
 
     }
 }
