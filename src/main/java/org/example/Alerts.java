5b9ed50ec85792710d066ac77eee6cf9ce2c00e8
mappuboy <pituvaraliyange@gmail.com>
2024-08-02 11:38:05 +0530

    Assert to compare the Actual and expected results

--- src/main/java/org/example/Alerts.java
+++ src/main/java/org/example/Alerts.java
@@ -3,6 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Alerts {
     public static void main(String[] args) throws InterruptedException {
@@ -16,7 +17,7 @@ public class Alerts {
         //Java script Alerts handling using "SwitchTo method
         // driver.switchTo().alert(); -> means switch you context browser to alert
         //Then it will check whether the alerts are available in the browser
-        //to accept the alert using "accept()" method
+        //to accept the alert using "accept()" method positive aspects -> "Ok"
 
         Thread.sleep(1000);
         //grab the text from the alert
@@ -24,8 +25,13 @@ public class Alerts {
         driver.switchTo().alert().accept();
 
         //click the negative like "No" or "Cancel"
+        driver.findElement(By.id("name")).sendKeys(text);
         driver.findElement(By.id("confirmbtn")).click();
+        Assert.assertEquals(driver.switchTo().alert().getText(),"Hello Navod, Are you sure you want to confirm?");
+
         driver.switchTo().alert().dismiss();
+
+       // Assert.assertEquals(driver.switchTo().alert().getText(),"Hello Navod, Are you sure you want to confirm?");
 
 
     }
